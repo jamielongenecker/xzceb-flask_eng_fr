9bbd3767e10f70560f49de895c1d740c16bf2963
jamielongenecker <jamiejlongenecker@gmail.com>
2022-04-29 17:31:45 +0000

    update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -6,18 +6,27 @@ app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
+    """
+    Flask server function for English to French translation
+    """
     text_to_translate = request.args.get('textToTranslate')
     # Write your code here
     return translator.english_to_french(text_to_translate)
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
+    """
+    Flask server function for French to English translation
+    """
     text_to_translate = request.args.get('textToTranslate')
     # Write your code here
     return translator.french_to_english(text_to_translate)
 
 @app.route("/")
 def renderIndexPage():
+    """
+    Flask server function to render the index page
+    """
     return render_template('index.html')
     # Write the code to render template
 
